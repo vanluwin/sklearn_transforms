5b4527b5d3864df231ae9997eccd6c09f2733c07
Richardson Santiago <richardsonsantiago@ufrn.edu.br>
2020-08-17 19:17:23 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,16 +1,49 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
 
-
-# All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
+    """Elimina Colunas não necessárias para o modelo"""
     def __init__(self, columns):
         self.columns = columns
 
-    def fit(self, X, y=None):
+    def fit(self, x, y=None):
+        return self
+    
+    def transform(self, x):
+        data = x.copy()
+
+        return data.drop(labels=self.columns, axis='columns')
+
+class MergeClasses(BaseEstimator, TransformerMixin):
+    """Junta as classes alvo de classificação"""
+    def __init__(self, target, new_value):
+        self.target = target
+        self.new_value = new_value
+
+    def fit(self, x, y=None):
         return self
 
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+    def transform(self, x):
+        data = x.copy()
+
+        return data.replace(self.target, self.new_value)
+
+class ReplaceMissingValues(BaseEstimator, TransformerMixin):
+    """ Substitui uma coluna com a média e a outra com zeros"""
+    def __init__(self, mean_column, zero_column):
+        self.mean_column = mean_column
+        self.zero_column = zero_column
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, x):
+        data = x.copy()
+
+        mean_imputer = SimpleImputer(strategy='mean')
+        zero_imputer = SimpleImputer(strategy='constant', fill_value=0)
+
+        data[[self.mean_column]] = mean_imputer.fit_transform(data[[self.mean_column]])
+        data[[self.zero_column]] = zero_imputer.fit_transform(data[[self.zero_column]])
+
+        return data
